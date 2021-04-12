2661e6b303933dff1d4973a6bd591ea35a9d9747
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-04-12 15:45:59 +0800

    feat(utils): implement text preprocessing

--- utils.py
+++ utils.py
@@ -1,6 +1,82 @@
 import os
 import sys
+import re
+import string
+from normalise import normalise, tokenize_basic
+
 
 def make_dir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
+
+
+def remove_hex(text):
+    """
+    Example: 
+    "\xe3\x80\x90Hello \xe3\x80\x91 World!"
+    """
+    res = []
+    i = 0
+    while i < len(text):
+        if text[i] == "\\" and i+1 < len(text) and text[i+1] == "x":
+            i += 3
+            res.append(" ")
+        else:
+            res.append(text[i])
+        i += 1
+    # text = text.encode('utf-8')
+    # text = text.encode('ascii', 'ignore')
+    # text = text.encode('ascii', errors='ignore')
+    # text = unicode(text)
+    # text = re.sub(r'[^\x00-\x7f]', r'', text)
+    # filter(lambda x: x in printable, text)
+    return "".join(res)
+
+def remove_punctuation(text):
+    return text.translate(str.maketrans('', '', string.punctuation))
+
+def remove_multiple_whitespace(text):
+    """
+    remove multiple whitespace
+    it covers tabs and newlines also
+    """
+    return re.sub(' +', ' ', text.replace('\n', ' ').replace('\t', ' ')).strip()
+
+
+def remove_punctuation(text):
+    return text.translate(str.maketrans('', '', string.punctuation))
+
+
+def normalize_text(text):
+    return " ".join(normalise(text, tokenizer=tokenize_basic, verbose=False))
+
+def substitute_word(text):
+    """
+    word subsitution to make it consistent
+    """
+    words = text.split(" ")
+    preprocessed = []
+    for w in words:
+        substitution = ""
+        if w == "mister":
+            substitution = "mr"
+        elif w == "missus":
+            substitution = "mrs"
+        else:
+            substitution = w
+        preprocessed.append(substitution)
+    return " ".join(preprocessed)
+
+def preprocess_text(text):
+    text = remove_hex(text)
+    text = remove_punctuation(text)
+    text = normalize_text(text)
+    # need to remove punctuation again as normalise sometimes add punctuation
+    text = remove_punctuation(text)
+    text = text.lower()
+    text = substitute_word(text)
+    text = remove_multiple_whitespace(text)
+    return text
+
+def create_filename_from_text(text): 
+    return "_".join(text.split(" "))
