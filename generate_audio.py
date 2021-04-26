2614d951169c7605b0c1017c71e4fb09a5677bf4
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-04-27 00:54:44 +0800

    refactor: modify path

--- generate_audio.py
+++ generate_audio.py
@@ -4,16 +4,18 @@ import random
 
 
 from constant import GOOGLE, RV, FESTIVAL, ESPEAK
+from constant import DATA_DIR, AUDIO_DIR, EXECUTION_TIME_DIR
 from tts import create_tts_by_name
-from utils import save_execution_time, make_dir
+from utils import save_execution_time, make_dir, read_json, set_seed
+
 
 
 def generate(tts_name: str, corpus_path: str, data_dir: str, execution_time_dir:str):
     tts = create_tts_by_name(tts_name)
     file = open(corpus_path)
     corpus = file.readlines()
-    audio_dir = os.path.join(data_dir, "audio")
-    execution_time_dir = os.path.join(execution_time_dir, "audio", tts_name)
+    audio_dir = os.path.join(data_dir, AUDIO_DIR)
+    execution_time_dir = os.path.join(execution_time_dir, AUDIO_DIR, tts_name)
     make_dir(execution_time_dir)
 
     i = 1
@@ -28,8 +30,8 @@ def generate(tts_name: str, corpus_path: str, data_dir: str, execution_time_dir:
         execution_time = end - start
         fpath = os.path.join(execution_time_dir, filename + ".txt")
         save_execution_time(fpath=fpath, execution_time=execution_time)
-        i += 1
         print(f"Generate {i}")
+        i += 1
         if tts_name in [GOOGLE]:
             random_number = float(random.randint(15, 40))/10.
             time.sleep(random_number)
@@ -38,10 +40,16 @@ def generate(tts_name: str, corpus_path: str, data_dir: str, execution_time_dir:
 
 
 if __name__ == "__main__" :
-    tts_name = GOOGLE
-    corpus_path = "corpus/europarl-20000.txt"
-    data_dir = "data/"
-    execution_time_dir = "execution_time/"
+    json_config_path = "config.json"
+    config = read_json(json_config_path)
 
+    set_seed(config["seed"])
+
+    corpus_path = config["input_corpus"]
+    output_dir = config["output_dir"]
+    data_dir = os.path.join(output_dir, DATA_DIR)
+    execution_time_dir = os.path.join(output_dir, EXECUTION_TIME_DIR)
+
+    tts_name = RV
     generate(tts_name, corpus_path, data_dir, execution_time_dir)
 
