0284d5f847ef4bbf86bf0f4d539943d2fed319ec
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-04-27 00:55:06 +0800

    feat: recognize audio

--- recognize_audio.py
+++ recognize_audio.py
@@ -4,7 +4,7 @@ import random
 
 from utils import save_execution_time, make_dir
 
-from utils import preprocess_text
+from utils import preprocess_text, read_json, set_seed
 
 ## constant for TTS
 from constant import GOOGLE, RV, ESPEAK, FESTIVAL
@@ -12,17 +12,19 @@ from constant import GOOGLE, RV, ESPEAK, FESTIVAL
 ## constant for ASR
 from constant import DS, DS2, W2L, WIT
 
+from constant import DATA_DIR, AUDIO_DIR, TRANSRCRIPTION_DIR, EXECUTION_TIME_DIR
+
 from asr import create_asr_by_name
 
 def recognize(tts_name: str, asr_name: str, data_dir: str, execution_time_dir: str):
     asr = create_asr_by_name(asr_name)
-    audio_dir = os.path.join(data_dir, "audio")
-    transcription_dir = os.path.join(data_dir, "transcription", tts_name)
-    execution_time_dir = os.path.join(execution_time_dir, "transcription", tts_name, asr_name)
+    audio_dir = os.path.join(data_dir, AUDIO_DIR)
+    transcription_dir = os.path.join(data_dir, TRANSRCRIPTION_DIR, tts_name)
+    execution_time_dir = os.path.join(execution_time_dir, TRANSRCRIPTION_DIR, tts_name, asr_name)
     make_dir(execution_time_dir)
 
-    for i in range(13229, 20000):
-    # for i in range(1, 3):
+    # for i in range(13229, 20000):
+    for i in range(0, 1):
         filename = f"{i}"
 
         print(f"Processing {i}")
@@ -43,9 +45,19 @@ def recognize(tts_name: str, asr_name: str, data_dir: str, execution_time_dir: s
 
 
 if __name__ == "__main__":
-    tts_name = RV
-    asr_name = WIT
-    data_dir = "data/"
-    execution_time_dir = "execution_time/"
+    
+    json_config_path = "config.json"
+    config = read_json(json_config_path)
 
-    recognize(tts_name, asr_name, data_dir, execution_time_dir)
+    set_seed(config["seed"])
+
+    corpus_path = config["input_corpus"]
+    output_dir = config["output_dir"]
+    data_dir = os.path.join(output_dir, DATA_DIR)
+    execution_time_dir = os.path.join(output_dir, EXECUTION_TIME_DIR)
+
+    tts_name = RV
+    
+    # for asr_name in [WIT, W2L, DS, DS2] :
+    for asr_name in [W2L]:
+        recognize(tts_name, asr_name, data_dir, execution_time_dir)
