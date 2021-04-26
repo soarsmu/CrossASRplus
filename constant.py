d590c81ae2d0e6de2124fb94d35bc68697b4c3dd
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-04-26 20:47:56 +0800

    config: add directories for saving outputs

--- constant.py
+++ constant.py
@@ -2,6 +2,13 @@ UNDETERMINABLE_TEST_CASE = 0
 FAILED_TEST_CASE = -1
 SUCCESSFUL_TEST_CASE = 1
 
+DATA_DIR = "data/"
+EXECUTION_TIME_DIR = "execution_time/"
+CASE_DIR = "case/"
+
+AUDIO_DIR = "audio/"
+TRANSRCRIPTION_DIR = "transcription/"
+
 
 ## TTS
 GOOGLE = "google"
