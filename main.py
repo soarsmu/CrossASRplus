d3162bd21ed25a26171bcad95c71d7efefd1d02e
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-04-26 20:48:25 +0800

    feat: create class by name

--- main.py
+++ main.py
@@ -10,13 +10,46 @@ from asr import create_asr_by_name
 
 if __name__ == "__main__":
 
-    json_config_path="config.json"
+    json_config_path = "config.json"
     config = read_json(json_config_path)
 
-    print(config)
-
     set_seed(config["seed"])
-    tts = createTTSbyName(config["tts"])
+    
+    tts = create_tts_by_name(config["tts"])
     asrs = []
     for asr_name in config["asrs"] :
-        asrs.append(createASRbyName(asrs))
+        asrs.append(create_asr_by_name(asrs))
+
+    file = open(corpus_path)
+    corpus = file.readlines()
+    
+    audio_dir = os.path.join(data_dir, "audio")
+    execution_time_dir = os.path.join(execution_time_dir, "audio", tts_name)
+
+    i = 0
+    # for i in range(len(corpus)) :
+    for i in range(10) :
+        text = corpus[i][:-1]
+        if config["recompute"] :
+            i
+            ## generate audio
+            ## save execution time
+        ## load execution time for generating audio
+
+        transcriptions = {}
+        for asr in asrs :
+            if config["recompute"] :
+                ## recognize audio
+                ## save transcription
+                ## save execution time
+            ## load transcription
+            ## postprocess transcriptions
+            ## load execution time for recognizing audio
+        
+        
+            
+            
+            
+
+
+
