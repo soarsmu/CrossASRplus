7375e7cc1d39e88842ebf378211e811576a46f93
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-04-27 13:31:00 +0800

    feat: get audio path from tts

--- tts.py
+++ tts.py
@@ -18,7 +18,15 @@ class TTS:
     def setName(self, name: str):
         self.name = name
 
-    def generateAudio(self, text:str, audiodir:str, filename:str):
+    def getAudioPath(self, text: str, audio_dir: str, filename: str):
+        base_dir = os.getcwd()
+        tts_dir = os.path.join(base_dir, audio_dir, self.name)
+        wavfile = os.path.join(tts_dir, filename + ".wav")
+
+        return os.path.relpath(wavfile, base_dir)
+
+
+    def generateAudio(self, text:str, audio_dir:str, filename:str):
         # abstract function need to be implemented by the child class
         raise NotImplementedError()
 
