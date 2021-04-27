4aeaf8e364e9b73331aa1eac2e61cbb46858ae0e
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-04-27 13:52:49 +0800

    refactor: add main program

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import json
 
@@ -5,7 +6,7 @@ from utils import set_seed, read_json
 
 from tts import create_tts_by_name
 from asr import create_asr_by_name
-
+from crossasr import CrossASR
 
 
 if __name__ == "__main__":
@@ -14,42 +15,21 @@ if __name__ == "__main__":
     config = read_json(json_config_path)
 
     set_seed(config["seed"])
-    
+
     tts = create_tts_by_name(config["tts"])
     asrs = []
-    for asr_name in config["asrs"] :
-        asrs.append(create_asr_by_name(asrs))
+    for asr_name in config["asrs"]:
+        asrs.append(create_asr_by_name(asr_name))
 
+    crossasr = CrossASR(tts=tts, asrs=asrs, output_dir=config["output_dir"])
+
+    corpus_path = config["input_corpus"]
     file = open(corpus_path)
     corpus = file.readlines()
+    texts = []
+    for text in corpus:
+        texts.append(text[:-1])
     
-    audio_dir = os.path.join(data_dir, "audio")
-    execution_time_dir = os.path.join(execution_time_dir, "audio", tts_name)
-
-    i = 0
-    # for i in range(len(corpus)) :
-    for i in range(10) :
-        text = corpus[i][:-1]
-        if config["recompute"] :
-            i
-            ## generate audio
-            ## save execution time
-        ## load execution time for generating audio
-
-        transcriptions = {}
-        for asr in asrs :
-            if config["recompute"] :
-                ## recognize audio
-                ## save transcription
-                ## save execution time
-            ## load transcription
-            ## postprocess transcriptions
-            ## load execution time for recognizing audio
-        
-        
-            
-            
-            
-
-
+    recompute = bool(config["recompute"])
+    crossasr.processCorpus(texts=texts, recompute=recompute)
 
