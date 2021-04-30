0c69e30878db942a7aa55437564465dfbf87c64e
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-04-30 11:57:08 +0800

    feat: implement wav2vec

--- asr.py
+++ asr.py
@@ -1,4 +1,7 @@
-import os
+import torch
+import soundfile as sf
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Tokenizer
+import os, gc
 import sys
 import subprocess
 
@@ -6,7 +9,7 @@ import subprocess
 from constant import GOOGLE, RV, ESPEAK, FESTIVAL
 
 ## constant for ASR
-from constant import DS, DS2, W2L, WIT
+from constant import DS, DS2, W2L, WIT, W2V
 
 from utils import make_dir
 
@@ -154,13 +157,43 @@ class Wit(ASR):
         # print(f"Wit transcription: {transcription}")
         return transcription
 
+class Wav2Vec2(ASR):
+    def __init__(self):
+        ASR.__init__(self, name=W2V)
+
+        # load pretrained model
+        self.tokenizer = Wav2Vec2Tokenizer.from_pretrained("facebook/wav2vec2-base-960h")
+        self.model = Wav2Vec2ForCTC.from_pretrained("facebook/wav2vec2-base-960h")
+        # self.device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+        # self.model.to(self.device)
+
+    def recognizeAudio(self, audio_path: str) -> str:
+        # load audio
+        audio_input, _ = sf.read(audio_path)
+
+        # transcribe
+        input_values = self.tokenizer(audio_input, return_tensors="pt").input_values
+        # input_values = input_values.to(self.device)
+
+        logits = self.model(input_values).logits
+        predicted_ids = torch.argmax(logits, dim=-1)
+        transcription = self.tokenizer.batch_decode(predicted_ids)[0]
+        # transcription = transcription.lower()
+        self.setTranscription(transcription)
+
+        del audio_input, input_values, logits,predicted_ids
+        torch.cuda.empty_cache()
+        gc.collect()
+
+        return transcription
 
 def create_asr_by_name(name: str):
     return {
         DS: DeepSpeech(),
         DS2: DeepSpeech2(),
         W2L: Wav2Letter(),
-        WIT: Wit()
+        WIT: Wit(),
+        W2V: Wav2Vec2()
     }[name]
 
 def test():
