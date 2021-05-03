3e0906dac3292be030630236bf46e0aeca0cc368
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-05-03 15:16:07 +0800

    feat: read corpus for consistent id

--- examples/generate_audio.py
+++ examples/generate_audio.py
@@ -3,24 +3,23 @@ import time
 import random
 
 from crossasr.constant import DATA_DIR, AUDIO_DIR, EXECUTION_TIME_DIR
-from crossasr.utils import save_execution_time, make_dir, read_json
+from crossasr.utils import save_execution_time, make_dir, read_json, read_corpus
 
 from utils import create_tts_by_name, set_seed
 
 def generate(tts_name: str, corpus_path: str, data_dir: str, execution_time_dir:str):
     tts = create_tts_by_name(tts_name)
-    file = open(corpus_path)
-    corpus = file.readlines()
+    
     audio_dir = os.path.join(data_dir, AUDIO_DIR)
     execution_time_dir = os.path.join(execution_time_dir, AUDIO_DIR, tts_name)
     make_dir(execution_time_dir)
 
-    i = 1
-    # for text in corpus :
-    for i in range(0, 1) :
-        text = corpus[i]
-        text = text[:-1]
-        filename = f"{i}"
+    corpus = read_corpus(corpus_path)
+
+    for i in range(0, 3) :
+        c = corpus[i]
+        text = c.getText()
+        filename = c.getId()
         start = time.time()
         tts.generateAudio(text=text, audio_dir=audio_dir, filename=filename)
         end = time.time()
@@ -33,9 +32,6 @@ def generate(tts_name: str, corpus_path: str, data_dir: str, execution_time_dir:
             random_number = float(random.randint(15, 40))/10.
             time.sleep(random_number)
 
-    file.close()
-
-
 if __name__ == "__main__" :
     json_config_path = "config.json"
     config = read_json(json_config_path)
@@ -47,6 +43,6 @@ if __name__ == "__main__" :
     data_dir = os.path.join(output_dir, DATA_DIR)
     execution_time_dir = os.path.join(output_dir, EXECUTION_TIME_DIR)
 
-    tts_name = "rv"
+    tts_name = config["tts"]
     generate(tts_name, corpus_path, data_dir, execution_time_dir)
 
