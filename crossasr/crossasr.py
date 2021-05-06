1dae038d96cd902959b40bf846189c0ef3f77ca4
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-05-06 09:45:57 +0800

    feat: dynamic chunking

--- crossasr/crossasr.py
+++ crossasr/crossasr.py
@@ -18,7 +18,7 @@ from jiwer import wer
 
 
 class CrossASR:
-    def __init__(self, tts: TTS, asrs: [ASR], output_dir: str, recompute=False, num_iteration=5, time_budget=3600, max_num_retry=0, text_batch_size=400, estimator=None) :
+    def __init__(self, tts: TTS, asrs: [ASR], output_dir: str, recompute=False, num_iteration=5, time_budget=3600, max_num_retry=0, text_batch_size=None, seed=None, estimator=None) :
         self.tts = tts
         self.asrs = asrs
         
@@ -38,6 +38,11 @@ class CrossASR:
         self.text_batch_size = text_batch_size
         self.estimator = estimator
         self.outputfile_failed_test_case = self.get_outputfile_for_failed_test_case()
+
+        if seed :
+            crossasr.utils.set_seed(seed)
+
+        ## TODO: convert print into global logging
         
     def init_directory(self) :
         # init directory for save the audio
@@ -49,7 +54,12 @@ class CrossASR:
 
     def get_outputfile_for_failed_test_case(self) :
         asrs_dir = "_".join([asr.getName() for asr in self.asrs])
-        result_dir = os.path.join(self.output_dir, "result", self.tts.getName(), asrs_dir, f"num_iteration_{self.num_iteration}",f"text_batch_size_{self.text_batch_size}")
+        result_dir = os.path.join(self.output_dir, 
+                                "result", 
+                                self.tts.getName(), 
+                                asrs_dir, 
+                                f"num_iteration_{self.num_iteration}", 
+                                f"text_batch_size_{self.text_batch_size if self.text_batch_size else 'global' }")
         make_dir(result_dir)
         experiment_name = f"with-estimator-{self.estimator.getName().replace('/','-')}" if self.estimator else "without-estimator"
         return os.path.join(result_dir, experiment_name)
@@ -209,7 +219,40 @@ class CrossASR:
 
         # print(f"Execution time: {execution_time}")
         return cases, execution_time
-    
+
+    def processOneIteration(self, curr_texts: [Text], processed_texts: [Text], cases):
+        start_time = time.time()
+        curr_cases = []
+
+        if self.estimator and len(processed_texts) > 0:
+            labels = get_labels_from_cases(cases)
+            self.trainEstimator(processed_texts, labels)
+            # print(f"Length texts: {len(curr_texts)}")
+            # start_time_classifier = time.time()
+            curr_texts = self.rank(curr_texts)
+            # end_time_classifier = time.time()
+            # print({f"Time for prediciton: {end_time_classifier-start_time_classifier}s"})
+
+        execution_time = 0.
+
+        i = 0
+        for text in curr_texts:
+            # print("================")
+            # print(f"{text.getId()}")
+            case, exec_time = self.processText(
+                text=text.getText(), filename=f"{text.getId()}")
+            curr_cases.append(case)
+            execution_time += exec_time
+            i += 1
+            if execution_time + time.time() - start_time > self.time_budget:
+                # print(f"Number of processed texts {i}")
+                break
+        
+        curr_processed_texts = curr_texts[:i]
+        unprocessed_texts = curr_texts[i:]
+
+        return curr_cases, curr_processed_texts, unprocessed_texts
+
     def processCorpus(self, texts: [Text]):
         # """
         # Run CrossASR on a corpus
@@ -218,32 +261,9 @@ class CrossASR:
         # return:
         # """
         # def processCorpus(self, text: [str], use_estimator: boolean, paremeters, FeatureExtractor, Classifier)
-
-        def processOneIteration(curr_texts: [Text], processed_texts: [Text], cases):
-            start_time = time.time()
-
-            if self.estimator and len(processed_texts) > 0:
-                labels = get_labels_from_cases(cases)
-                self.trainEstimator(processed_texts, labels)
-                curr_texts = self.rank(curr_texts)
-
-            execution_time = 0.
-            
-            i = 0
-            for text in curr_texts :
-                # print("================")
-                # print(f"{text.getId()}")
-                case, exec_time = self.processText(text=text.getText(), filename=f"{text.getId()}")
-                cases.append(case)
-                execution_time += exec_time
-                i += 1
-                if execution_time + time.time() - start_time > self.time_budget :
-                    # print(f"Number of processed texts {i}")
-                    break
-            processed_texts.extend(curr_texts[:i])
-
         
-        max_size = len(texts)
+        remaining_texts = texts
+        curr_texts = []
         processed_texts = []
         cases = []
         num_failed_test_cases = []
@@ -252,20 +272,36 @@ class CrossASR:
         for asr in self.asrs:
             num_failed_test_cases_per_asr[asr.getName()] = []
         
-        for i in range(self.num_iteration): 
-            lower_bound = i*self.text_batch_size
-            upper_bound = min((i+1)*self.text_batch_size, max_size)
-            if lower_bound < upper_bound :
-                curr_texts = texts[lower_bound:upper_bound]
-                processOneIteration(curr_texts, processed_texts, cases)
+        for i in range(self.num_iteration):
+            # print(f"Iteration: {i+1}")
+            
+            if self.text_batch_size :
+                curr_texts = remaining_texts[:self.text_batch_size]
+                remaining_texts = remaining_texts[self.text_batch_size:]
+            else : # use global visibility
+                curr_texts = remaining_texts
+
+            if len(curr_texts) > 0 :
+                
+                curr_cases, curr_processsed_texts, unprocessed_texts = self.processOneIteration(curr_texts, processed_texts, cases)
+                cases.extend(curr_cases)
+                processed_texts.extend(curr_processsed_texts)
+                if self.text_batch_size :
+                    remaining_texts.extend(unprocessed_texts)
+                else :
+                    remaining_texts = unprocessed_texts
+
                 num_failed_test_cases.append(calculate_cases(cases, mode=FAILED_TEST_CASE))
-                for asr in self.asrs :
+                for asr in self.asrs:
                     num_failed_test_cases_per_asr[asr.getName()].append(calculate_cases_per_asr(
                         cases, mode=FAILED_TEST_CASE, asr_name=asr.getName()))
                 num_processed_texts.append(len(processed_texts))
             else :
-                print("Texts is not enough")
-
+                print("Texts are not enough!")
+            
+            ### shuffle the remaining texts
+            np.random.shuffle(remaining_texts)
+        
         data = {}
         data["number_of_failed_test_cases_all"] = num_failed_test_cases
         data["number_of_failed_test_cases_per_asr"] = num_failed_test_cases_per_asr
