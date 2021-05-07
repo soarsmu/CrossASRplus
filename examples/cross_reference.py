19935ec957e5545aada4a4958039a9b05c48ed2c
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-05-07 11:03:53 +0800

    feat: read json config from command line

--- examples/cross_reference.py
+++ examples/cross_reference.py
@@ -2,21 +2,22 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action='ignore', category=UserWarning)
 
+import sys
 import utils
 
 from crossasr import CrossASR
 
-
 if __name__ == "__main__":
     
-    config = utils.readJson("config-trial.json")
+    config = utils.readJson(sys.argv[1]) # read json configuration file
 
     tts = utils.getTTS(config["tts"])
     asrs = utils.getASRS(config["asrs"])
     
-    crossasr = CrossASR(tts=tts, asrs=asrs, output_dir=config["output_dir"], recompute=config["recompute"])
+    crossasr = CrossASR(tts=tts, asrs=asrs, output_dir=config["output_dir"], recompute=True)
 
     text = "software engineering conference"
     filename = "software_engineering_conference"
+
     crossasr.processText(text=text, filename=filename)
     crossasr.printResult(text=text, filename=filename)
--- examples/test_asr.py
+++ examples/test_asr.py
@@ -2,19 +2,20 @@ import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 warnings.simplefilter(action='ignore', category=UserWarning)
 
-import os
+import os, sys
 from crossasr import CrossASR
 import json
 import utils
 
 if __name__ == "__main__":
 
-    config = utils.readJson("config-demo.json")
+    config = utils.readJson(sys.argv[1]) # read json configuration file
 
     tts = utils.getTTS(config["tts"])
     asrs = utils.getASRS(config["asrs"])
+    estimator = utils.getEstimator(config["estimator"])
 
-    crossasr = CrossASR(tts=tts, asrs=asrs, **utils.parseConfig(config))
+    crossasr = CrossASR(tts=tts, asrs=asrs, estimator=estimator, **utils.parseConfig(config))
 
     corpus_fpath = os.path.join(config["output_dir"], config["corpus_fpath"])
     texts = utils.readCorpus(corpus_fpath=corpus_fpath)
