990fbe40d51da0dbc243f0c6ac2f323b11cc0012
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-05-07 10:50:48 +0800

    refactor: change the order

--- examples/pool.py
+++ examples/pool.py
@@ -1,13 +1,13 @@
-from tts.rv import ResponsiveVoice
 from tts.google import Google
 from tts.espeak import Espeak
 from tts.festival import Festival
+from tts.rv import ResponsiveVoice
 
 from asr.deepspeech import DeepSpeech
 from asr.deepspeech2 import DeepSpeech2
 from asr.wav2letter import Wav2Letter
-from asr.wit import Wit
 from asr.wav2vec2 import Wav2Vec2
+from asr.wit import Wit
 
-tts_pool = [ResponsiveVoice(), Google(), Espeak(), Festival()]
+tts_pool = [Google(), Espeak(), Festival(), ResponsiveVoice()]
 asr_pool = [Wav2Vec2(), DeepSpeech(), DeepSpeech2(), Wav2Letter(), Wit()]
