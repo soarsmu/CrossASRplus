588527ac0994e976c88450487ab673a8053310d2
Muhammad Hilmi Asyrofi <mhilmiasyrofi@gmail.com>
2021-12-28 12:56:38 +0800

    define Mac tts in tts pool

--- examples/pool.py
+++ examples/pool.py
@@ -2,6 +2,7 @@ from tts.google import Google
 from tts.espeak import Espeak
 from tts.festival import Festival
 from tts.rv import ResponsiveVoice
+from tts.mac import Mac
 
 from asr.deepspeech import DeepSpeech
 from asr.deepspeech2 import DeepSpeech2
@@ -9,5 +10,5 @@ from asr.wav2letter import Wav2Letter
 from asr.wav2vec2 import Wav2Vec2
 from asr.wit import Wit
 
-tts_pool = [Google(), Espeak(), Festival(), ResponsiveVoice()]
+tts_pool = [Google(), Espeak(), Festival(), ResponsiveVoice(), Mac()]
 asr_pool = [Wav2Vec2(), DeepSpeech(), DeepSpeech2(), Wav2Letter(), Wit()]
